05b76b2de9f4db69ce92dcdcd00ace6eaa73b22b
GustavoM7 <gustavosmarquesf@gmail.com>
2019-07-19 22:44:38 -0300

    resposta EP2

--- EP2/resposta.js
+++ EP2/resposta.js
@@ -1,13 +1,27 @@
-    // Importação do FileSystem
-	const fs = require("fs");
-	
-	// Função de leitura de arquivo
-	fs.readFile("teste.txt", 'utf8', (err, data)=>{
-		//...
+// Importação do FileSystem
+const fs = require("fs");
+
+function transfer(leitorCallback, escritorCallback){
+	console.log("Processando dados...")
+	leitorCallback(escritorCallback);
+}
+
+
+// Função de leitura de arquivo
+function leitor(escritorCallback){
+		fs.readFile("teste.txt", 'utf8', (err, data)=>{
+		if (err) throw err;
+		console.log("Salvando dados...");
+		escritorCallback(data)
 	});
-	//...
-	
-	// Função de escrita de arquivo
-	fs.writeFile("destino.txt", DADOSAQUI, ()=>{
-		//...
+}
+
+// Função de escrita de arquivo
+function escritor(DADOSAQUI){
+	fs.writeFile("destino.txt", DADOSAQUI, (err)=>{
+		if (err) throw err;
+  		console.log('Arquivo foi salvo!');
 	});
+}
+
+transfer(leitor, escritor);
